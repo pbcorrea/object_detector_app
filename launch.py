ae9b03cdb613ab56c75242c1a5cbaf414674ac76
pedropablocorrea <pbcorrea@uc.cl>
2019-02-18 12:56:50 -0300

    Added error logging and connection restart

--- launch.py
+++ launch.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
+import datetime
 
-height='480'
-width='600'
+height='600'
+width='800'
 size = str(width+'x'+height)
 camera = "left"
 quality = "40"
-fps = "15.0"
+fps = "25.0"
 
 ip_dir=("http://10.23.170.23/control/faststream.jpg?stream=full&preview&previewsize="
 +size+"&quality="+quality+"&fps="+fps+"&camera="+camera)
 
-operative = False
-while operative == False:
+log_directory = 'logs/'
+while True:
     try:
         os.system('python object_detection_multithreading.py -strin '+ip_dir)
-        operative = True
-        break
-    except:
-        operative = False
+    except Exception as e:
+        f=open(log_directory+'error_logs.txt', "a+")
+        f.write('{}. Failure to execute. Error:\t{}'.format(datetime.now.strftime("%d/%m/%Y, %H:%M:%S"),str(e)))
+        pass
