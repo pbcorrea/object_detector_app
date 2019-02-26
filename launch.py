bf270ff4e7c75d6026851b82e1a2ea69a78f2b88
pedropablocorrea <pbcorrea@uc.cl>
2019-02-26 13:05:07 -0300

    Changed logs directory

--- launch.py
+++ launch.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 import os
 import datetime
+import subprocess
 from subprocess import Popen, PIPE
 import time
 
 cwd = os.getcwd()
 log_directory = os.path.join(cwd,'logs')
-print(log_directory)
-print(log_directory+'error_logs.txt')
+log_file = os.path.join(log_directory,'error_logs.txt')
 while True:
     try:
         subprocess.call(['python','oddl.py'])
         #os.system('python object_detection_multithreading.py -strin '+ip_dir)
     except Exception as e:
-        f=open(log_directory+'error_logs.txt', "a+")
-        f.write('{}. Failure to execute. Error:\t{}'.format(datetime.now.strftime("%d/%m/%Y, %H:%M:%S"),str(e)))
+        f=open(log_file, "a+")
+        f.write('{}. Failure to execute. Error:\t{}'.format(datetime.datetime.now.strftime("%d/%m/%Y, %H:%M:%S"),str(e)))
         pass
