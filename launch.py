e9ace9eaea06754ee3f9cf709690ce8d13df9e72
pedropablocorrea <pbcorrea@uc.cl>
2019-02-26 15:59:22 -0300

    Error handling modified. Now using Popen

--- launch.py
+++ launch.py
@@ -3,17 +3,21 @@
 import os
 import datetime
 import subprocess
-from subprocess import Popen, PIPE
+from subprocess import PIPE, CalledProcessError, check_call, Popen
 import time
 
 cwd = os.getcwd()
 log_directory = os.path.join(cwd,'logs')
 log_file = os.path.join(log_directory,'error_logs.txt')
-while True:
-    try:
-        subprocess.call(['python','oddl.py'])
-        #os.system('python object_detection_multithreading.py -strin '+ip_dir)
-    except Exception as e:
-        f=open(log_file, "a+")
-        f.write('{}. Failure to execute. Error:\t{}'.format(datetime.datetime.now.strftime("%d/%m/%Y, %H:%M:%S"),str(e)))
-        pass
+with open(log_file, "a+") as f:
+    while True:
+        try:
+            program = Popen(['python','oddl.py'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = program.communicate()
+            ret = program.returncode
+            if ret:
+                print('Error {} with code {}'.format(stderr,ret))
+        except Exception as e:
+            f.write('{}. Failure to execute. Error:\t{}'.format(datetime.datetime.now.strftime("%d/%m/%Y, %H:%M:%S"),str(e)))
+            exit(0)
+            pass
