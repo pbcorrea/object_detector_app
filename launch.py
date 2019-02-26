2141417d1d9b320a2984ef15bd39dbfa2e99f730
pedropablocorrea <pbcorrea@uc.cl>
2019-02-26 16:37:14 -0300

    added streaming output

--- launch.py
+++ launch.py
@@ -12,12 +12,13 @@ log_file = os.path.join(log_directory,'error_logs.txt')
 with open(log_file, "a+") as f:
     while True:
         try:
-            program = Popen(['python','oddl.py'], stdout=PIPE, stderr=PIPE)
-            stdout, stderr = program.communicate()
+            process = Popen(['python','oddl.py'], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process.communicate()
             ret = program.returncode
+            print(stdout)
             if ret:
+                process.terminate()
                 print('Error {} with code {}'.format(stderr,ret))
         except Exception as e:
             f.write('{}. Failure to execute. Error:\t{}'.format(datetime.datetime.now.strftime("%d/%m/%Y, %H:%M:%S"),str(e)))
-            exit(0)
             pass
