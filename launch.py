27cada1be5c04935d69ea293a4fd8a87574d84eb
pedropablocorrea <pbcorrea@uc.cl>
2019-02-27 16:59:46 -0300

    Added non-blocking stdout reader as a class.

--- launch.py
+++ launch.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import time
 import datetime
 import subprocess
 from subprocess import PIPE, CalledProcessError, check_call, Popen
-import time
+from app_utils import NonBlockingStreamReader as NBSR
 
 cwd = os.getcwd()
 log_directory = os.path.join(cwd,'logs')
@@ -12,11 +13,14 @@ log_file = os.path.join(log_directory,'error_logs.txt')
 with open(log_file, "a+") as f:
     while True:
         try:
-            process = Popen(['python','oddl.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-            stdout, stderr = process.communicate()
+            process = Popen(['python','oddl.py'], stdout=PIPE, stderr=PIPE)
             poll = process.poll()
-            ret = process.returncode
+            #ret = process.returncode
+            nbsr = NBSR(process.stdout)
             while poll == None:
+                stdout = nbsr.readline(0.1)
+                if not stdout:
+                    break
                 print(stdout.decode("utf-8"))
             else:
                 print('Error {} with code {}'.format(stderr,ret))
