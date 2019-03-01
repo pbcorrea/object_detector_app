fddcfe7d626fbf2d1b73e18181ec87371d5a79fc
pedropablocorrea <pbcorrea@uc.cl>
2019-03-01 16:55:59 -0300

    Changed path into absolute

--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
 @echo off
 title ODDL Launching...
-cd .\oddl\object_detector_app
+cd C:Users\RMCLABS\oddl\object_detector_app
 conda activate oddl
 echo Virtualenv activated
 echo Starting service...
