6aeed25dab623bb0fd1601b91d7246adacf552c8
pedropablocorrea <pbcorrea@uc.cl>
2019-03-05 18:15:08 -0300

    Changed alarm sound

--- oddl.py
+++ oddl.py
@@ -39,7 +39,7 @@ category_index = {1: {'id': 1, 'name': 'person'}, 2: {'id': 2, 'name': 'bicycle'
  8: {'id': 8, 'name': 'truck'}, 9: {'id': 9, 'name': 'boat'}}
 
 def raise_alarm(frame, connection, alarm):
-    alarm_request_ip = 'http://10.23.170.23/control/rcontrol?action=sound&soundfile=Alarm'
+    alarm_request_ip = 'http://10.23.170.23/control/rcontrol?action=sound&soundfile=Beep'
     alarm_time = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.localtime())
     if alarm:
         try:
