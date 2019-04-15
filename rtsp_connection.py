81975109c4db752d9300aa419b1791743b856bc0
pedropablocorrea <pbcorrea@uc.cl>
2019-04-15 15:44:22 -0400

    Changed rtsp connection

--- rtsp_connection.py
+++ rtsp_connection.py
@@ -1,13 +1,18 @@
 import cv2
+import numpy as np
 
-stream_ip=str('rtsp://admin:12345678@10.10.10.10/stream0/mobotix.mjpeg')
+stream_ip=str('rtsp://admin:12345678@10.10.10.10/stream0/mobotix.h264')
 
 while True:
-    cap = cv.VideoCapture(stream_ip)
+    cap = cv2.VideoCapture(stream_ip)
     ret, frame = cap.read()
-    cv2.imshow('Recording',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if ret == False:
+        print("Frame is empty")
         break
+    else:
+        cv2.imshow('Recording',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 print('Video OK')
 cap.release()
 cv2.destroyAllWindows()
